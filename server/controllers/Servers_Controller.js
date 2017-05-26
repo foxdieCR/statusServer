fdfed5e6be0b75c42f59f4559d3ffb0961e5c40d
lalfaro <lalfaro@soin.co.cr>
2017-05-26 17:55:05 -0600

    finalizacion del CRUD de servidores

--- server/controllers/Servers_Controller.js
+++ server/controllers/Servers_Controller.js
@@ -1,9 +1,21 @@
 'use strict'
 
-const companies = require('../models/Companies')
+const serversModel = require('../models/Servers')
 
 function getServer (req, res) {
-
+	serversModel.findById(req.params.id, function (err, serverData) {
+		if (!err) {
+			console.log('Server found')
+			res.status(200).json(
+				serverData
+			);
+		} else {
+			console.log('ERROR: ' + err)
+			res.status(500).json({
+				error: 'ERROR: ' + err
+			})
+		}
+	});
 }
 
 function getAll (req, res) {
@@ -11,15 +23,84 @@ function getAll (req, res) {
 }
 
 function saveServer (req, res) {
+	const data = new serversModel({
+		environment: req.body.environment,
+		uri: req.body.uri,
+		port: req.body.port,
+		https: req.body.https,
+		time: req.body.time,
+		path: req.body.path
+	})
 
+	data.save(function (err) {
+		if (!err) {
+			console.log('Server Saved')
+			res.status(200).json({
+				id: data._id,
+				environment: req.body.environment,
+				uri: req.body.uri,
+				port: req.body.port,
+				https: req.body.https,
+				time: req.body.time,
+				path: req.body.path
+			})
+		} else {
+			console.log('ERROR: ' + err)
+			res.status(500).json({
+				error: 'ERROR: ' + err
+			})
+		}
+	})
 }
 
 function updateServer (req, res) {
-
+	const findBy = {
+		_id: req.params.id
+	}
+	const data = {
+		environment: req.body.environment,
+		uri: req.body.uri,
+		port: req.body.port,
+		https: req.body.https,
+		time: req.body.time,
+		path: req.body.path
+	}
+	serversModel.findOneAndUpdate(findBy, data, function (err) {
+		if (!err) {
+			console.log('Server updated')
+			res.status(200).json({
+				id: req.params.id,
+				environment: req.body.environment,
+				uri: req.body.uri,
+				port: req.body.port,
+				https: req.body.https,
+				time: req.body.time,
+				path: req.body.path
+			});
+		} else {
+			console.log('ERROR: ' + err)
+			res.status(500).json({
+				error: 'ERROR: ' + err
+			})
+		}
+	})
 }
 
 function deleteServer (req, res) {
-
+	serversModel.remove({ _id: req.params.id }, function (err) {
+		if (!err) {
+			console.log('Server deleted')
+			res.status(200).json({
+				id: req.params.id,
+				message: "Servidor eliminado."
+			});
+		} else {
+			console.log('ERROR: ' + err)
+			res.status(500).json({
+				error: 'ERROR: ' + err
+			})
+		}
+	})
 }
 
 module.exports = {
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,11 @@
 
 const express = require('express')
 const companyRoutes = require('./company.route')
+const serverRoutes = require('./server.route')
 
 const router = express.Router()
 
 router.use('/companies', companyRoutes)
+router.use('/servers', serverRoutes)
 
 module.exports = router
