9a7bc22d7d0de800f9b3d0cead40a0fcb44428ad
Luis Emmanuel Alfaro <emmalfca@gmail.com>
2018-04-27 15:15:36 -0600

    arreglo de bug

--- server/models/Companies.js
+++ server/models/Companies.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
-const { Schema } = mongoose.Schema
-const { ObjectId } = Schema.ObjectId
+const { Schema } = mongoose
+const { ObjectId } = Schema
 
 const companiesSchema = Schema({
   token: 'String',
